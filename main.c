7c84a02883024c2d683a7bcfeda0892e9f6b9520
g0sh06 <s.atanasov51@gmail.com>
2024-10-06 11:01:08 +0200

    All of the methods are implemented

--- main.c
+++ main.c
@@ -12,14 +12,89 @@ typedef struct NODE {
 } Node;
 
 typedef struct HashTable{ 
-  Node* table[TABLE_SIZe];
+  Node* table[TABLE_SIZE];
 } HashTable;
 
+// this is the algorithm of declaring where a noded should be placed
+int hashFunction(int key) {
+  return key % TABLE_SIZE;
+}
 
+//create a new node
+Node* createNode(int key, int value) {
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->key = key;
+  newNode->value = value;
+  newNode->next = NULL;
+  return newNode;
+}
 
+//function that inserts node into the hash table
+void insert(HashTable* ht, int key, int value) {
+  int hashIndex = hashFunction(key);
+  Node* current = ht->table[hashIndex];
+
+  // check if the key exiss, update value if it is found
+  while(current != NULL) {
+    if(current->key == key) {
+	current->value = value;
+	return;
+  }
+    current = current->next;
+
+   //create node and insert it
+    Node* newNode = createNode(key, value);
+    newNode->next = ht->table[hashIndex];
+    ht->table[hashIndex] = newNode;
+}
+
+int search(HashTable* ht, int key) {
+  Node* current = ht->table[hashFunction(key)];
+  while(current != NULL) {
+    if(current->key == key) current->value;
+    current = current->next;
+  }
+  return -1;
+}
+
+void delete(HashTable* ht, int key){
+  int hashIndex = hashFunction(key);
+  Node* current = ht->table[hashIndex];
+  Node* prev = NULL;
+
+  while(current != NULL) {
+    if(current->key == key) {
+      if(prev == NULL) ht->table[hashIndex] = current->next;
+      else prev->next = current->next;
+      free(current);
+      return;
+    }
+     prev = current;
+     current = current->next;
+}
+
+void display(HashTable* ht) {
+  for(int i =0; i < TABLE_SIZE; i++) {
+    Node* current = ht->table[i];
+    printf("Index %d: ", i);
+    while(current != NULL) {
+      printf("(%d, %d) ->" current->key, current->value);
+      current = current->next;
+    }
+    printf("NULL\n");
+}
 
 int main() {
- printf("d");
+ HashTable ht = {NULL};
 
+ insert(&ht, 1, 10);
+ insert(&ht, 11, 20);
+ insert(&ht, 21, 30);
+ insert(&ht, 41, 45);
+
+ printf("Hash Table\n");
+ display(&ht);
+
+ return 0;
 }
 
